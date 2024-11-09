20bd1d2b8ad2d004b4579340c10cc5c82b9938fe
aFeiCQUT <2039787966@qq.com>
2024-11-09 18:39:24 +0800

    更新汇总答题文件

--- 10.24第二次课作业/T2.jl
+++ 10.24第二次课作业/T2.jl
@@ -31,20 +31,19 @@ end
 # println("样条插值所得结果:")
 # println(join(map(x -> @sprintf("%.6f", x), caq_spline), "\n"))
 
+# 第一幅图————纯享数据散点图
 figure()
-hold(true) # 确保后续绘制不会覆盖之前的图形
+hold(true)
 scatter(t, cA, 10, "k", label="原始数据")
 scatter(tq, cAq_linear, 20, "b", marker="h", filled=true, label="线性插值")
 scatter(tq, cAq_spline, 10, "g", marker="d", filled=true, label="样条插值")
-
-# 添加图例
 legend()
 
-# 拟合一阶动力学方程
+# 准备拟合数据
 ln_cA = log.(cA)  # 计算自然对数
 
-# 使用线性回归拟合 ln(cA) = kt + C
-lin_fit = fit("poly", t, ln_cA, order=1)
+# 拟合一阶动力学方程，这里用线性回归 ln(cA) = kt + C
+lin_fit = fit("poly1", t, ln_cA) # 或者lin_fit = fit("poly", t, ln_cA, order=1)
 params = lin_fit.params
 
 # 提取 k 和 C
@@ -56,7 +55,23 @@ println("拟合参数:")
 println("k: ", k)
 println("C: ", C)
 
-# 绘制拟合曲线
-fit_curve = exp.(k .* t .+ C)  # 恢复到原始形式 cA = e^(kt + C)
-plot(t, fit_curve, color="m", label="拟合曲线")
+# 第二幅图————绘制对数拟合曲线
+figure()
+plotfit(lin_fit, t, ln_cA)
+title("对数拟合lncA-t")
+xlabel("t")
+ylabel("ln(cA)")
 legend()
+
+# 第三幅图————绘制对数拟合还原后的曲线
+figure()
+hold(true) # 确保后续绘制不会覆盖之前的图形
+scatter(t, cA, 10, "k", label="原始数据")
+scatter(tq, cAq_linear, 20, "b", marker="h", filled=true, label="线性插值")
+scatter(tq, cAq_spline, 10, "g", marker="d", filled=true, label="样条插值")
+fit_curve = exp.(k .* t .+ C)  # 恢复到原始形式 cA = e^(kt + C)
+plot(t, fit_curve, color="m")
+title("还原后的拟合曲线cA-t")
+xlabel("t")
+ylabel("cA")
+legend();
